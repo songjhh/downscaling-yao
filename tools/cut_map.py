0a250ea199659ad3c1c29a34dde06f69119c12e3
songjhh <songjhh0312@gmail.com>
2024-03-13 16:33:56 +0800

    .

---
+++ tools/cut_map.py
@@ -0,0 +1,24 @@
+import xarray
+import geopandas as gpd
+from shapely.geometry import mapping
+import os
+import matplotlib.pyplot as plt
+import rioxarray
+
+root_dir = os.path.abspath("../data/2020-month/")
+changjiang_shp_path = os.path.abspath("../data/shp/changjiang.shp")
+result_dir = os.path.abspath("../data/2020-changjiang-month/")
+
+for root, dirs, files in os.walk(root_dir):
+    for file in files:
+        xds = xarray.open_dataset(os.path.join(root, file))
+        xds = xds[["precipitation"]].transpose("time", "lat", "lon")
+        xds.rio.set_spatial_dims(x_dim="lon", y_dim="lat", inplace=True)
+        xds.rio.write_crs("EPSG:4326", inplace=True)
+
+        geodf = gpd.read_file(r"%s" % changjiang_shp_path)
+        clipped = xds.rio.clip(geodf.geometry.apply(mapping), geodf.crs)
+        clipped.to_netcdf(result_dir + "/" + file, mode="w", format="NETCDF4")
+        clipped_ds = xarray.open_dataset(result_dir + "/" + file, decode_times=False)
+        clipped_ds.attrs = xds.attrs
+        clipped_ds.close()
--- tools/watch_file.py
+++ tools/watch_file.py
@@ -1,19 +1,34 @@
 import os
 from netCDF4 import Dataset as ncdataset
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import xarray
 
-root_dir = os.path.abspath("../data/2020-month/")
+root_dir = os.path.abspath("../data/2020-changjiang-month/")
+# root_dir = os.path.abspath("../data/2020-month/")
+pic_dir = os.path.abspath("../data/temp/")
 
 for root, dirs, files in os.walk(root_dir):
     for file in files:
-        dst = ncdataset(os.path.join(root, file))
-        precipitation_var = dst.variables["precipitation"]
-        print(precipitation_var)
-        print(precipitation_var[0, 0, 0])
-        print(precipitation_var[0, 0, 1])
-        print(precipitation_var[0, 1, 0])
-        print(precipitation_var[0, 1, 1])
-        # print(dst.variables["precipitation"])
-        # print(np.max(dst.variables["precipitation"]))
-        # print(dst.variables["precipitation"].isel(time=0))
-        break
+
+        plt.close("all")
+        xds = xarray.open_dataset(os.path.join(root, file))
+        target = xds["precipitation"]
+        plt.figure(figsize=(32, 12))
+        target.transpose("time", "lat", "lon").plot()
+        # plt.show()
+        plt.savefig(pic_dir + "/" + file[:-4] + ".png")
+
+        # dst = ncdataset(os.path.join(root, file))
+        # target = dst.variables["precipitation"]
+        # target = np.squeeze(target)
+        # target = np.flipud(target)
+        # sns.set()
+        # plt.close("all")
+        # plt.figure(figsize=(32, 12))
+        # ax = sns.heatmap(
+        #     target, vmin=0, yticklabels=False, xticklabels=False, vmax=np.max(target)
+        # )
+        # plt.show()
+        # plt.savefig(pic_dir + "/" + file[:-4] + ".png")
